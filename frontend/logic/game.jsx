c8f47d7f70607317632e175c0af07f66535df37f
robtaussig <robert.taussig@gmail.com>
2016-09-07 14:29:46 -0400

    Work on findAttackers

--- frontend/logic/game.jsx
+++ frontend/logic/game.jsx
@@ -6,7 +6,7 @@ let board = {
     [['b-r'],['b-n'],['b-b'],['b-q'],['b-k'],['b-b'],['b-n'],['b-r']],
     [['b-p'],['b-p'],['b-p'],['b-p'],['b-p'],['b-p'],['b-p'],['b-p']],
     [['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l']],
-    [['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l']],
+    [['b-q'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['w-q']],
     [['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l']],
     [['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l'],['n-l']],
     [['w-p'],['w-p'],['w-p'],['w-p'],['w-p'],['w-p'],['w-p'],['w-p']],
@@ -33,13 +33,125 @@ export function setSelected(piece) {
   board.selectedSquare = piece.pos;
 }
 
-export function canMove(toX, toY) {
-  let pos = board.selectedSquare;
-  let piece = board.pieces[pos[1]][pos[0]][0];
-  let color = board.pieces[pos[1]][pos[0]][0][0];
-  console.log(piece);
+export function canMove(toX, toY, pieces = board.pieces, from = board.selectedSquare) {
+  if (notInCheck(toX, toY, from)) {
+    if (checkMove(toX, toY, pieces, from)) return true;
+  }
+  return false;
+}
+
+function checkPawnCaptures (toX, toY) {
+  const [x, y] = board.selectedSquare;
+  const color = board.pieces[y][x][0][0];
+  const dX = toX - x;
+  const dY = toY - y;
+  if (color === 'w' && dY > 0 || color === 'b' && dY< 0) {
+    return false;
+  } else if (Math.abs(dX) === 1 && Math.abs(dY) === 1 &&
+    board.pieces[toY][toX][0][0] !== color &&
+    board.pieces[toY][toX][0][0] !== 'n') {
+      return true;
+    }
+  return false;
+}
+
+function notInCheck (toX, toY, from = board.selectedSquare) {
+  let color = board.pieces[from[1]][from[0]][0][0];
+  let testBoard = dupe(board.pieces);
+  let afterMove = testMove(testBoard, from, [toX, toY]);
+  let king = findKing(afterMove,color);
+  let attackers = findAttackers(king);
+  return true;
+  // let [x, y] = from;
+  // let color = board.pieces[y][x][0][0];
+  // let testBoard = dupe(board.pieces);
+  // let afterMove = testMove(testBoard, [x, y], [toX, toY]);
+  // let king = findKing(afterMove,color);
+  // let oppPieces = findAllPieces(afterMove,color === 'w' ? 'b' : 'w');
+  // let legalOppMoves = findLegalMoves(afterMove,oppPieces);
+  // if (threatenedKing(legalOppMoves,king)) {
+  //   debugger
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+}
+
+function threatenedKing (moves, king) {
+  for (let i=0; i<moves.length;i++) {
+    if (JSON.stringify(moves[i]) === JSON.stringify(king)) return true;
+  }
+  return false;
+}
+
+function testMove (testBoard, from, to) {
+  let selected = from;
+  let start = testBoard[selected[1]][selected[0]][0];
+  testBoard[to[1]][to[0]][0] = start;
+  testBoard[selected[1]][selected[0]][0] = 'nil';
+  return testBoard;
+}
+
+function dupe (array) {
+  const duped = [[],[],[],[],[],[],[],[]];
+  for (let i=0;i<array.length;i++) {
+    for (let j=0;j<array[i].length;j++) {
+      let piece = `${array[i][j][0][0]}-${array[i][j][0][2]}`;
+      duped[i].push([piece]);
+    }
+  }
+  return duped;
+}
+
+function findKing (afterMove,color) {
+  const target = `${color}-k`;
+  for (let i=0; i<=7;i++) {
+    for (let j=0; j<=7;j++) {
+      if (afterMove[i][j][0] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+function findAllPieces (afterMove,color) {
+  const allPieces = [];
+  for (let i=0; i<=7;i++) {
+    for (let j=0; j<=7;j++) {
+      if (afterMove[i][j][0][0] === color) {
+        allPieces.push([j, i]);
+      }
+    }
+  }
+  return allPieces;
+}
+
+function findLegalMoves (afterMove, pieces) {
+  const legalMoves = [];
+  pieces.forEach(piece => {
+    legalMovesByPiece(afterMove,piece).forEach(el=>legalMoves.push(el));
+  });
+  return legalMoves;
+}
+
+function legalMovesByPiece (afterMove,piece) {
+  const legalPieceMoves = [];
+  for (let i=0; i<=7;i++) {
+    for (let j=0; j<=7;j++) {
+      if (checkMove(j, i, afterMove, piece)) {
+        legalPieceMoves.push([j, i]);
+      }
+    }
+  }
+  return legalPieceMoves;
+}
+
+function checkMove (toX, toY, afterMove, from) {
+  let pos = from;
+  let piece = afterMove[pos[1]][pos[0]][0];
+  let color = afterMove[pos[1]][pos[0]][0][0];
   if (piece[2] === 'n') {
-    if (board.pieces[toY][toX][0][0] !== color) {
+    if (afterMove[toY][toX][0][0] !== color) {
       return knightMoves(pos,toX, toY);
     }
   } else if (piece[2] === 'b') {
@@ -65,22 +177,6 @@ export function canMove(toX, toY) {
       return pawnMoves(toX, toY);
     }
   }
-  return false;
-}
-
-function checkPawnCaptures (toX, toY) {
-  const [x, y] = board.selectedSquare;
-  const color = board.pieces[y][x][0][0];
-  const dX = toX - x;
-  const dY = toY - y;
-  if (color === 'w' && dY > 0 || color === 'b' && dY< 0) {
-    return false;
-  } else if (Math.abs(dX) === 1 && Math.abs(dY) === 1 &&
-    board.pieces[toY][toX][0][0] !== color &&
-    board.pieces[toY][toX][0][0] !== 'n') {
-      return true;
-    }
-  return false;
 }
 
 function checkPawnObstruction (toX, toY) {
@@ -136,14 +232,11 @@ function pawnMoves (toX, toY) {
     (y === 6 && dy === -2 && dx === 0) ||
     (y === 1 && dy === -2 && dx === 0)
   );
-
 }
 
 export function move(toX, toY) {
   let selected = board.selectedSquare;
-  console.log(selected);
   let start = board.pieces[selected[1]][selected[0]][0];
-  console.log(start);
   board.pieces[toY][toX][0] = start;
   board.pieces[selected[1]][selected[0]][0] = 'nil';
 
