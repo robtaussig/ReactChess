fbdfb213389ae73096350b62a949fe211a01ba5f
robtaussig <robert.taussig@gmail.com>
2016-09-08 23:23:11 -0400

    Setup basic AI

--- logic/board_node.js
+++ logic/board_node.js
@@ -11,6 +11,7 @@ function findBestMove (board,specMoves,depth) {
   let bestMove = null;
   moves.forEach(move => {
     let currentNode = new BoardNode(move,board,specMoves,depth,0,null,[]);
+    // console.log(currentNode.score);
     bestMove = bestMove ?
       (currentNode.score > bestMove.score ?
         currentNode : bestMove) : currentNode;
@@ -114,6 +115,7 @@ class BoardNode {
     let otherScore = otherPieces
                     .map(piece=> this.evalPiecePositionalValue(piece,board))
                     .reduce((sum,el) => sum + el);
+    // console.log(ownScore,otherScore);
     return ownScore - otherScore;
   }
 
@@ -148,46 +150,79 @@ class BoardNode {
     let posX = pawn.pos[0],
       posY = pawn.pos[1],
       value = 0,
-      i = 0,
       left = posX === 0 ? null : (posX - 1),
       right = posX === 7 ? null : (posX + 1),
       yDir = pawn.side === 'w' ?
         (posY === 0 ? null : (posY - 1)) : (posY === 7 ? null : (posY + 1));
 
     if (left && yDir) {
-      if (board[yDir][left][0][0] === pawn.side) {
-        value += (PIECE_VALUES[board[yDir][left][0][0]] / 5);
+      if (board[yDir][left][0][0] === pawn.side &&
+        board[yDir][left][0][2] === 'p') {
+        value += 15;
       }
     }
     if (right && yDir) {
-      if (board[yDir][right][0][0] === pawn.side) {
-        value += (PIECE_VALUES[board[yDir][right][0][0]] / 5);
+      if (board[yDir][right][0][0] === pawn.side &&
+        board[yDir][right][0][2] === 'p') {
+        value += 15;
       }
     }
-
     value += (
-      20 - Math.round((Math.abs(3.5 - posY) * (Math.abs(3.5 - posX))))
+      20 * (0.25 / Math.abs(3.5 - posY) * (Math.abs(3.5 - posX)))
     );
     return value;
   }
 
   rookPositionalValue (rook,board) {
-    return 0;
+    let moves = findAllLegalMovesByPiece (rook,board,this.specialMoves);
+    return moves.length * 5;
   }
 
   knightPositionalValue (knight,board) {
-    return 0;
+    let value = 0;
+    let moves = findAllLegalMovesByPiece (knight,board,this.specialMoves);
+    moves.forEach(move => {
+      value += (
+        15 * (0.25 / Math.abs(3.5 - move[1][1]) * (Math.abs(3.5 - move[1][0])))
+      );
+    });
+    return value + moves.length * 5;
   }
 
   bishopPositionalValue (bishop,board) {
-    return 0;
+    let value = 0;
+    let moves = findAllLegalMovesByPiece (bishop,board,this.specialMoves);
+
+    return moves.length * 5;
   }
 
   queenPositionalValue (queen,board) {
-    return 0;
+    let value = 0;
+    let moves = findAllLegalMovesByPiece (queen,board,this.specialMoves);
+
+    return moves.length * 5;
   }
 
   kingPositionalValue (king,board) {
-    return 0;
+    let posX = king.pos[0],
+      posY = king.pos[1],
+      value = 0,
+      left = posX === 0 ? null : (posX - 1),
+      right = posX === 7 ? null : (posX + 1),
+      yDir = king.side === 'w' ?
+      (posY === 0 ? null : (posY - 1)) : (posY === 7 ? null : (posY + 1));
+
+    [left, posX, right].forEach(xCoord => {
+      let testSquare = board[yDir][xCoord][0];
+      if (testSquare[0] === king.side) {
+        if (testSquare[2] === 'p') {
+          value += 30;
+        } else {
+          value += 20;
+        }
+      }
+    });
+
+    return value;
   }
 }
